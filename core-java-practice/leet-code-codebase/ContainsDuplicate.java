5c26afe9f18af9c40bf0884c16b4d3e0779e217f
Brajdeep Singh <brajdeepsingh8172@gmail.comgit config --global user.email brajdeepsingh8172@gmail.com>
2026-06-10 09:54:16 +0530

    [Brajdeep] Add leetcode LC 217 solution

---
+++ core-java-practice/leet-code-codebase/ContainsDuplicate.java
@@ -0,0 +1,17 @@
+#LC  217 Contains Duplicate
+
+import java.util.Arrays;
+
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                return true;
+            }
+        }
+ 
+        return false;
+    }
+}
